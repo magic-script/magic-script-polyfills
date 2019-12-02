2e5d8ba5fe557a9aca31ed87aa79f27af119d5d6
Luis Caro <lcaro_cht@magicleap.com>
2019-12-02 11:23:02 -0500

    Upgraded xhr implementation

--- src/xhr.js
+++ src/xhr.js
@@ -1,20 +1,61 @@
 import { fetch } from './fetch.js';
 import { Evented } from './evented.js';
 
+// XHR States
+const UNSENT = 0;
+const OPENED = 1;
+const DONE = 4;
+
+// Extends from Evented to fire events
 export class XMLHttpRequest extends Evented {
-  open (method, url) {
+  constructor() {
+    super();
+    this.headers = {};
+    this.readyState = UNSENT;
+    this.withCredentials = true;
+  }
+
+  // Stores method and url
+  open(method, url) {
+    this.readyState = OPENED;
     this.method = method;
     this.url = url;
   }
-  send () {
-    // print('XHR', this.method, this.url);
-    fetch(this.url, { method: this.method })
+
+  /**
+   * Resets status and values
+   * Needed by some implementations
+   */
+  abort() {
+    this.readyState = UNSENT;
+    this.responseText = '';
+    this.response = null;
+    this.status = null;
+  }
+
+  // Append header to headers object
+  setRequestHeader(name, value) {
+    this.headers[name] = value;
+  }
+
+  // Returns all headers
+  getAllResponseHeaders() {
+    return this.responseHeaders;
+  }
+
+  /**
+   * Uses fetch to fire request
+   * Fires both load and readystatechange so registered components know data was loaded
+   */
+  send(body) {
+    fetch(this.url, { method: this.method, body })
       .then(res => {
+        this.readyState = DONE;
         this.status = res.status;
-        // print('XHR', this.status, this.url);
         this.statusText = res.statusText;
         this.redirected = res.redirected;
-        this.headers = res.headers;
+        this.responseHeaders = res.headers;
+
         if (this.responseType === 'arraybuffer') {
           this.bodyField = 'response';
           return res.arrayBuffer();
@@ -23,9 +64,22 @@ export class XMLHttpRequest extends Evented {
           return res.text();
         }
       })
-      .then(body => {
-        this[this.bodyField] = body;
-        this.emit('load', {});
-      }, err => this.emit('error', err));
+      .then(
+        body => {
+          this.response = body;
+          this[this.bodyField] = body;
+          this.emit('readystatechange', body);
+          this.emit('load', body);
+        },
+        err => {
+          this.abort();
+          this.emit('error', err);
+        }
+      );
+  }
+
+  // Get response header by key
+  getResponseHeader(key) {
+    return this.responseHeaders.get(key);
   }
 }
