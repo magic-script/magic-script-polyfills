bc775cf8ae11adc5ae74ef788806d83db4b211ea
Tim Caswell <tcaswell@magicleap.com>
2019-12-17 15:28:39 -0600

    Add empty stream class polyfill

---
+++ src-ts/node/stream.ts
@@ -0,0 +1,8 @@
+import { EventEmitter } from "./events.js";
+
+export class Stream extends EventEmitter {
+    constructor() {
+        super();
+        throw new Error("TODO: Implement stream.Stream");
+    }
+}
