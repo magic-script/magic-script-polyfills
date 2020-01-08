15f22a5895f2d9c6f007c619c918b0033e963ee2
Tim Caswell <tcaswell@magicleap.com>
2020-01-08 12:44:03 -0600

    Add polyfills for crypto random using fs + /dev/urandom

--- src-ts/node/crypto.ts
+++ src-ts/node/crypto.ts
@@ -1,3 +1,66 @@
+import { close, open, read } from "../fs-promised.js";
+import { closeSync, OnResult, openSync, readSync } from "../fs-uv.js";
+
 export function createHash() {
     throw new Error("TODO: Implement crypto.createHash for node");
+}
+
+export function randomBytes(size: number): Uint8Array;
+export function randomBytes(size: number, callback: OnResult<Uint8Array>): void;
+export function randomBytes(size: number, callback?: OnResult<Uint8Array>): Uint8Array | void {
+    const buffer = new Uint8Array(size);
+    if (callback) {
+        randomBytesCallback(buffer, callback);
+    } else {
+        return randomBytesSync(buffer);
+    }
+}
+
+function randomBytesCallback(buffer: Uint8Array, callback: OnResult<Uint8Array>): void {
+    randomBytesPromised(buffer).then((data) => callback(null, data), (err) => callback(err));
+}
+
+async function randomBytesPromised(buffer: Uint8Array): Promise<Uint8Array> {
+    const fd = await open("/dev/urandom", "r", 0o400);
+    try {
+        await read(fd, buffer, 0);
+    } finally {
+        await close(fd);
+    }
+    return buffer;
+}
+
+function randomBytesSync(buffer: Uint8Array): Uint8Array {
+    const fd = openSync("/dev/urandom", "r", 0o400);
+    try {
+        readSync(fd, buffer, 0);
+    } finally {
+        closeSync(fd);
+    }
+    return buffer;
+}
+
+export function randomFillSync(buffer: Uint8Array, rawOffset?: number, rawSize?: number): Uint8Array {
+    randomBytesSync(normalizeOffsetBuffer(buffer, rawOffset, rawSize));
+    return buffer;
+}
+
+export function randomFill(buffer: Uint8Array, offset: number, size: number, callback: OnResult<Uint8Array>): void;
+export function randomFill(buffer: Uint8Array, offset: number, callback: OnResult<Uint8Array>): void;
+export function randomFill(buffer: Uint8Array, callback: OnResult<Uint8Array>): void;
+export function randomFill(
+    buffer: Uint8Array,
+    rawOffset: number | OnResult<Uint8Array>,
+    rawSize?: number | OnResult<Uint8Array>,
+    rawCallback?: OnResult<Uint8Array>): void {
+    randomBytesCallback(
+        normalizeOffsetBuffer(buffer, rawOffset, rawSize),
+        (rawOffset || rawSize || rawCallback) as OnResult<Uint8Array>);
+}
+
+function normalizeOffsetBuffer(buffer: Uint8Array, rawOffset: any, rawSize: any) {
+    const offset = typeof rawOffset === "number" ? rawOffset : 0;
+    const size = typeof rawSize === "number" ? rawSize : buffer.length - offset;
+    return (offset === 0 && size === buffer.length)
+        ? buffer : buffer.subarray(offset, offset + size);
 }
