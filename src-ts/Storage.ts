4cd133e8bf74a67ee0450e54cb854a0cd35b5b9c
Tim Caswell <tcaswell@magicleap.com>
2020-02-10 11:33:27 -0600

    Finish initial persistent version of Storage

--- src-ts/Storage.ts
+++ src-ts/Storage.ts
@@ -1,73 +1,78 @@
-import { Fs, fs } from "uv";
-import { readfileSync } from "./fs-sync.js";
+import { mkdirSync } from "./fs-uv.js";
+import { readFileSync, writeFileSync, readdirSync, unlinkSync } from "./node/fs.js";
+import { pathJoin } from "./utils2.js";
 
 export class Storage {
     private base: string;
+    private data: Map<string, string>;
 
     constructor(basePath?: string) {
-        try {
-            fs.mkdir(new Fs(), basePath, 0o700);
-        } catch (err) {
-            if (!/^EEXIST/.test(err.message)) { throw err; }
+        this.updateBase(basePath);
+    }
+
+    public updateBase(basePath?: string) {
+        if (!basePath) {
+            this.data = new Map();
+            this.base = undefined;
+        } else {
+            try {
+                mkdirSync(basePath, 0o700);
+            } catch (err) {
+                if (!/^EEXIST/.test(err.message)) { throw err; }
+            }
+            this.data = undefined;
+            this.base = basePath;
         }
-        this.base = basePath;
     }
+
     public setItem(name: string, value: string) {
-
+        if (this.base) {
+            writeFileSync(pathJoin(this.base, String(name)), String(value));
+        } else {
+            this.data.set(String(name), String(value));
+        }
     }
 
-    public getItem(name: string): string {
+    public getItem(name: string): string | null {
+        if (this.base) {
+            try {
+                return readFileSync(pathJoin(this.base, String(name)), 'utf8');
+            }
+            catch (err) {
+                if (!/^ENOENT/.test(err.message)) { throw err; }
+                return null;
+            }
+        } else {
+            const stringKey = String(name);
+            if (this.data.has(stringKey)) {
+                return String(this.data.get(stringKey));
+            }
+            return null;
+        }
+    }
 
+    public removeItem(name: string) {
+        if (this.base) {
+            try {
+                unlinkSync(pathJoin(this.base, String(name)));
+            } catch (err) {
+                if (!/^ENOENT/.test(err.message)) { throw err; }
+            }
+        } else {
+            const stringKey = String(name);
+            this.data.delete(stringKey);
+
+        }
     }
 
     public clear(): void {
-
-    }
-
-}
-
-/**
- * To mimic the native implementation as close as possible
- * the class was called 'Storage' and carries the data in a map().
- */
-export class Storage {
-    constructor() {
-        this.data = new Map();
-    }
-
-    // If key exists get entry from map by key
-    public getItem(key) {
-        const stringKey = String(key);
-        if (this.data.has(stringKey)) {
-            return String(this.data.get(stringKey));
+        if (this.base) {
+            for (const name of readdirSync(this.base)) {
+                unlinkSync(pathJoin(this.base, name));
+            }
+        } else {
+            this.data.clear();
         }
-        return null;
     }
 
-    // Set entry with a string key identifier
-    public setItem(key, val) {
-        this.data.set(String(key), String(val));
-    }
-
-    // Remove entry by key
-    public removeItem(key) {
-        const stringKey = String(key);
-        this.data.delete(stringKey);
-    }
-
-    // Clear all entries from localStorage
-    public clear() {
-        this.data.clear();
-    }
-
-    // Get key by index
-    public key(index) {
-        const arr = Array.from(this.data.keys());
-        return arr[index];
-    }
-
-    // Get quantity of entries in localStorage
-    get length() {
-        return this.data.size;
-    }
 }
---
+++ tests/test-storage.js
@@ -0,0 +1,33 @@
+import { Storage } from "../src/index.js";
+import { readfileSync } from "../src/fs-sync.js";
+import { assert, utf8Decode } from "../src/utils2.js";
+import "../src/polyfills.js";
+const memStorage = new Storage();
+const fileStorage = new Storage('storage');
+
+const meta = JSON.parse(utf8Decode(readfileSync("package.json", "r", 0o644)));
+console.log(meta);
+for (const key in meta) {
+    console.log({ [key]: fileStorage.getItem(key) });
+}
+memStorage.clear();
+fileStorage.clear();
+for (const key in meta) {
+    const value = JSON.stringify(meta[key], null, 2) + "\n";
+    assert(memStorage.getItem(key) === null);
+    assert(fileStorage.getItem(key) === null);
+    memStorage.setItem(key, value);
+    fileStorage.setItem(key, value);
+    assert(memStorage.getItem(key) === value);
+    assert(fileStorage.getItem(key) === value);
+    memStorage.removeItem(key);
+    fileStorage.removeItem(key);
+    assert(memStorage.getItem(key) === null);
+    assert(fileStorage.getItem(key) === null);
+    memStorage.removeItem(key);
+    fileStorage.removeItem(key);
+    memStorage.setItem(key, value);
+    fileStorage.setItem(key, value);
+    memStorage.setItem(key, value);
+    fileStorage.setItem(key, value);
+}
